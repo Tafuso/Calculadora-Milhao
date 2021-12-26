ce99e2c3b38bb3031e749d941f565f1ac6110b70
Tafuso <gustavovasquez2002@gmail.com>
2021-12-26 18:00:44 -0300

    building javascript

---
+++ index.js
@@ -0,0 +1,33 @@
+var acumulado = 0
+var investimentoInicial = 0
+var investimentoMensal = 0
+var taxaJuros = 0
+const resposta = document.querySelector("#resultado")
+
+function pegarValor() {
+ investimentoInicial = parseFloat(document.querySelector("#valor-inicial").value)
+ investimentoMensal = parseFloat(document.querySelector("#valor-mensal").value)
+ taxaJuros = parseFloat(document.querySelector("#rentabilidade").value)/12
+
+ if (taxaJuros > 0 && (investimentoInicial > 0|| investimentoMensal > 0)) {
+   calcular(investimentoInicial, investimentoMensal, taxaJuros)
+ } else if (investimentoMensal > 0) {
+    acumulado = investimentoInicial || 0
+    for (var tempo = 0; acumulado < 1000000; tempo++) {
+      acumulado += investimentoMensal
+    } 
+    resposta.innerHTML = `${(tempo/12).toFixed(2)} anos`
+ } else {
+  resposta.innerHTML = "--"
+ }
+}
+
+function calcular(x, y, z) {
+  acumulado = x || 0
+  y = y || 0
+  for (var tempo = 0; acumulado < 1000000; tempo++) {
+    acumulado = (acumulado*z/100) + acumulado
+    acumulado += y    
+  } 
+  resposta.innerHTML = `${(tempo/12).toFixed(2)} anos`
+}
